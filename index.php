961ba14ee2f976f50ee5ce4e98194183d1d457f0
UmbertoV <umberto.ottone@gmail.com>
2020-01-21 21:17:37 +0100

    Inizializzazione fogli da lavoro

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+ <html lang="it" dir="ltr">
+     <head>
+         <meta charset="utf-8">
+         <!--  FOGLIO CSS -->
+         <link rel="stylesheet" href="public/css/app.css">
+
+     <title> Google FAQs</title>
+     </head>
+     <body>
+
+     </body>
+ </html>
